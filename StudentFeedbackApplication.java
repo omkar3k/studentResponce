f03cf5440c866f439a381ef08e6c7e8e2ec467f2
Nikam Sujal <145206707+omkar3k@users.noreply.github.com>
2024-10-07 16:44:20 +0530

    Add files via upload

---
+++ StudentFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.klu.studentFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentFeedbackApplication.class, args);
+	}
+
+}
